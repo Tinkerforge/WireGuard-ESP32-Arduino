Tinkerforge/WireGuard-ESP32-Arduino

commit f6efb943cf1e8a4b01eb3c3d570526c71f71f0bc

Refactor wireguardif_init error handling. No functional change.

src/wireguardif.c
@@ -947,87 +947,89 @@ err_t wireguardif_init(struct netif *netif) {
 	wireguard_init();
 	log_i(TAG "wireguard module initialized.");
 
-	if (netif && netif->state) {
+	if (!netif || !netif->state) {
+		log_e(TAG "netif or state is NULL: netif=%p, netif.state:%p", netif, netif ? netif->state : NULL);
+		return ERR_ARG;
+	}
 
-		// The init data is passed into the netif_add call as the 'state' - we will replace this with our private state data
-		init_data = (struct wireguardif_init_data *)netif->state;
+	// The init data is passed into the netif_add call as the 'state' - we will replace this with our private state data
+	init_data = (struct wireguardif_init_data *)netif->state;
 
-		// Clear out and set if function is successful
-		netif->state = NULL;
+	// Clear out and set if function is successful
+	netif->state = NULL;
 
-		if (wireguard_base64_decode(init_data->private_key, private_key, &private_key_len)
-				&& (private_key_len == WIREGUARD_PRIVATE_KEY_LEN)) {
+	if (!wireguard_base64_decode(init_data->private_key, private_key, &private_key_len)
+			|| (private_key_len != WIREGUARD_PRIVATE_KEY_LEN)) {
+		log_e(TAG "invalid init_data private key");
+		return ERR_ARG;
+	}
 
-			udp = udp_new();
+	udp = udp_new();
 
-			if (udp) {
-				result = udp_bind(udp, IP_ADDR_ANY, init_data->listen_port); // Note this listens on all interfaces! Really just want the passed netif
-				if (result == ERR_OK) {
-					device = (struct wireguard_device *)mem_calloc(1, sizeof(struct wireguard_device));
-					if (device) {
-						device->netif = netif;
-						device->underlying_netif = underlying_netif;
-						//udp_bind_netif(udp, underlying_netif);
+	if (!udp) {
+		log_e(TAG "failed to allocate UDP");
+		result = ERR_MEM;
+	}
 
-						device->udp_pcb = udp;
-						device->in_filter_fn = init_data->in_filter_fn;
-						device->out_filter_fn = init_data->out_filter_fn;
-						log_d(TAG "start device initialization");
-						// Per-wireguard netif/device setup
-						uint32_t t1 = wireguard_sys_now();
-						if (wireguard_device_init(device, private_key)) {
-							uint32_t t2 = wireguard_sys_now();
-							log_d(TAG "Device init took %ums\r\n", (t2-t1));
+	result = udp_bind(udp, IP_ADDR_ANY, init_data->listen_port); // Note this listens on all interfaces! Really just want the passed netif
+	if (result != ERR_OK) {
+		log_e(TAG "failed to bind UDP err=%d", result);
+		goto cleanup_udp;
+	}
+
+	device = (struct wireguard_device *)mem_calloc(1, sizeof(struct wireguard_device));
+	if (!device) {
+		log_e(TAG "failed to allocate device context.");
+		result = ERR_MEM;
+		goto cleanup_udp;
+	}
+
+	device->netif = netif;
+	device->underlying_netif = underlying_netif;
+	//udp_bind_netif(udp, underlying_netif);
+
+	device->udp_pcb = udp;
+	device->in_filter_fn = init_data->in_filter_fn;
+	device->out_filter_fn = init_data->out_filter_fn;
+	log_d(TAG "start device initialization");
+	// Per-wireguard netif/device setup
+	uint32_t t1 = wireguard_sys_now();
+	if (!wireguard_device_init(device, private_key)) {
+		log_e(TAG "failed to initialize WireGuard device.");
+		result = ERR_ARG;
+		goto cleanup_device;
+	}
+
+	uint32_t t2 = wireguard_sys_now();
+	log_d(TAG "Device init took %ums\r\n", (t2-t1));
 
 #if LWIP_CHECKSUM_CTRL_PER_NETIF
-							NETIF_SET_CHECKSUM_CTRL(netif, NETIF_CHECKSUM_ENABLE_ALL);
+	NETIF_SET_CHECKSUM_CTRL(netif, NETIF_CHECKSUM_ENABLE_ALL);
 #endif
-							netif->state = device;
-							netif->name[0] = 'w';
-							netif->name[1] = 'g';
-							netif->output = wireguardif_output;
-							netif->linkoutput = NULL;
-							netif->hwaddr_len = 0;
-							netif->mtu = WIREGUARDIF_MTU;
-							// We set up no state flags here - caller should set them
-							// NETIF_FLAG_LINK_UP is automatically set/cleared when at least one peer is connected
-							netif->flags = 0;
+	netif->state = device;
+	netif->name[0] = 'w';
+	netif->name[1] = 'g';
+	netif->output = wireguardif_output;
+	netif->linkoutput = NULL;
+	netif->hwaddr_len = 0;
+	netif->mtu = WIREGUARDIF_MTU;
+	// We set up no state flags here - caller should set them
+	// NETIF_FLAG_LINK_UP is automatically set/cleared when at least one peer is connected
+	netif->flags = 0;
 
-							udp_recv(udp, wireguardif_network_rx, device);
+	udp_recv(udp, wireguardif_network_rx, device);
 
-							// Start a periodic timer for this wireguard device
-							sys_timeout(WIREGUARDIF_TIMER_MSECS, wireguardif_tmr, device);
+	// Start a periodic timer for this wireguard device
+	sys_timeout(WIREGUARDIF_TIMER_MSECS, wireguardif_tmr, device);
 
-							result = ERR_OK;
-						} else {
-							log_e(TAG "failed to initialize WireGuard device.");
-							mem_free(device);
-							device = NULL;
-							udp_remove(udp);
-							result = ERR_ARG;
-						}
-					} else {
-						log_e(TAG "failed to allocate device context.");
-						udp_remove(udp);
-						result = ERR_MEM;
-					}
-				} else {
-					log_e(TAG "failed to bind UDP err=%d", result);
-					udp_remove(udp);
-				}
+	return ERR_OK;
 
-			} else {
-				log_e(TAG "failed to allocate UDP");
-				result = ERR_MEM;
-			}
-		} else {
-			log_e(TAG "invalid init_data private key");
-			result = ERR_ARG;
-		}
-	} else {
-		log_e(TAG "netif or state is NULL: netif=%p, netif.state:%p", netif, netif ? netif->state : NULL);
-		result = ERR_ARG;
-	}
+cleanup_device:
+	mem_free(device);
+	device = NULL;
+
+cleanup_udp:
+	udp_remove(udp);
 	return result;
 }
 

commit 5ccdc909f18ad0db371b5c9cd4a9abf1d67fcb1f

Fix udp alloc failure not returning. Broken in f6efb943 (last commit)

src/wireguardif.c
@@ -968,7 +968,7 @@ err_t wireguardif_init(struct netif *netif) {
 
 	if (!udp) {
 		log_e(TAG "failed to allocate UDP");
-		result = ERR_MEM;
+		return ERR_MEM;
 	}
 
 	result = udp_bind(udp, IP_ADDR_ANY, init_data->listen_port); // Note this listens on all interfaces! Really just want the passed netif